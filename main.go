06d9c8f3f5d6162b68fd576b2f254664bb468a30
Yaroslav <proskurin.yarik@gmail.com>
2025-05-14 14:50:53 +0200

    feat: add contactHandler and mapped it to /contact URL

--- main.go
+++ main.go
@@ -3,24 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "Welcome to my awesome site!")
-	fmt.Println("First request handled:", time.Now())
-
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>")
+}
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	fmt.Println("Build time:", time.Now())
-
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
