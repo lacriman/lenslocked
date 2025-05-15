5adbe6cea6eaba1a121d22d6964dce0d0227474f
Yaroslav <proskurin.yarik@gmail.com>
2025-05-15 11:21:03 +0200

    feat: add FAQ page

--- main.go
+++ main.go
@@ -15,6 +15,28 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>FAQ page</h1>
+
+		<div style="margin-bottom: 40px;">
+			<p><strong>Q:</strong> Is there a free version?</p>
+			<p><strong>A:</strong> Yes, we offer a free trial for 30 days on any paid plans.</p>
+		</div>
+
+		<div style="margin-bottom: 40px;">
+			<p><strong>Q:</strong> What are your support hours?</p>
+			<p><strong>A:</strong> We have support staff answering emails 24/7, though response times may be slower on weekends.</p>
+		</div>
+
+		<div style="margin-bottom: 40px;">
+			<p><strong>Q:</strong> How do I contact support?</p>
+			<p><strong>A:</strong> Email us at <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>.</p>
+		</div>
+	`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -34,12 +56,14 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
-func main() {  
+func main() {
 	var router Router
 	fmt.Println("Starting the server on port 3000...")
 	http.ListenAndServe(":3000", router)
