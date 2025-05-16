4760270a60ad643148850ccddbd3e9849b816f81
Yaroslav <proskurin.yarik@gmail.com>
2025-05-16 09:01:32 +0200

    refactor(homeHandler): use HTML template for rendering

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "Welcome to my awesome site!")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,11 +59,11 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-	r.Get("/faq/{faqID}", func (w http.ResponseWriter, r *http.Request)  {
-		faqID:= chi.URLParam(r, "faqID")
+	r.Get("/faq/{faqID}", func(w http.ResponseWriter, r *http.Request) {
+		faqID := chi.URLParam(r, "faqID")
 		fmt.Fprintf(w, "Faq ID: %s", faqID)
 	})
-	r.NotFound(func (w http.ResponseWriter, r *http.Request)  {
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
