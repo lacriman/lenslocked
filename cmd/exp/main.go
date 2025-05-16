ab1ca648bf2eca0b6df70d44d2244b4c900c832b
Yaroslav <proskurin.yarik@gmail.com>
2025-05-16 11:34:13 +0200

    feat: pass User struct to template for rendering

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,36 +1,53 @@
 package main
 
 import (
-	"os"
+	"log"
+	"net/http"
 	"text/template"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type User struct {
-	Name string
-	Age  int
-	Meta UserMeta
+	Name   string
+	Number string
+	Email  string
 }
 
-type UserMeta struct {
-	Visits int
+func executeTemplate(w http.ResponseWriter, filepath string, user interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	//  Used filepath.Join(...) to avoid platform-specific path issues — super important for cross-platform deployments
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, user)
+	if err != nil {
+		log.Printf("executing template %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Name:   "John Smith",
+		Number: "0141224123",
+		Email:  "y.proskurin@rto.de",
+	}
+	executeTemplate(w, "user.gohtml", user)
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
 
-	user := User{
-		Name: "John Smith",
-		Age:  111,
-		Meta: UserMeta{
-			Visits: 4,
-		},
-	}
+	r := chi.NewRouter()
+	r.Get("/", userHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":3000", r)
 }
